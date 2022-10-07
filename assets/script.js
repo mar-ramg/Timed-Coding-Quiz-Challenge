9b51f0b173a01b6e57b598530bb802b303c283a2
who cares <mariaaramirez80@gmail.com>
2022-10-07 12:49:30 -0700

    added id to my HTML for JavaScript

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var header = document.querySelector("#header");
+var scoresBtn = document.querySelector("scores-button");
+// var timer = document.querySelector("#time"); //
+
+var welcome  = document.querySelector("#intro");
+var startBtn = document.querySelector("#start-button");
+
+var questionScreen = document.querySelector("#question-page");
+var question = document.querySelector("#question");
+
+var reactButton = document.querySelectorAll(".choices");
+var answerBtn1 = document.querySelector("#answer-button-1");
+var answerBtn2 = document.querySelector("#answer-button-2");
+var answerBtn3 = document.querySelector("#answer-button-3");
+var answerBtn4 = document.querySelector("#answer-button-4");
+var answerCheck = document.querySelector("#check-answer");
+
+var completeScreen = document.querySelector("#complete-screen");
+var score = document.querySelector("#final-score");
+var userInitial = document.querySelector("#enter-initials");
+var submitBtn = document.querySelector("#submit-button");
+
+var highscorePage = document.querySelector("#highscore-page");
+var scoreRecord = document.querySelector("#score-record");
+var backBtn = document.querySelector("#back-button");
+var clearBtn = document.querySelector("#clear-button");
+
