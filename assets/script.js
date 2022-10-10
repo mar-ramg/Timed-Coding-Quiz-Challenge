0a8fae002da29365df01103218a600602310b037
who cares <mariaaramirez80@gmail.com>
2022-10-10 11:39:55 -0700

    Added the rest of the eventListeners on JS, changed my website look on CSS, and added more ids to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,61 @@
-var header = document.querySelector("#header");
-var scoresBtn = document.querySelector("scores-button");
-// var timer = document.querySelector("#time"); //
+var welcome = document.querySelector("#introduction");
+var startBtn = document.querySelector("#start_button");
+var introPage =document.querySelector("#intro_page");
 
-var welcome  = document.querySelector("#intro");
-var startBtn = document.querySelector("#start-button");
+var questionPage = document.querySelector("#question_page");
+var askQuestion = document.querySelector("#ask_question");
 
-var questionScreen = document.querySelector("#question-page");
-var question = document.querySelector("#question");
+var reactButtons = document.querySelectorAll(".choices");
+var answerBtn1 = document.querySelector("#answer_btn1");
+var answerBtn2 = document.querySelector("#answer_btn2");
+var answerBtn3 = document.querySelector("#answer_btn3");
+var answerBtn4 = document.querySelector("#answer_btn4");
 
-var reactButton = document.querySelectorAll(".choices");
-var answerBtn1 = document.querySelector("#answer-button-1");
-var answerBtn2 = document.querySelector("#answer-button-2");
-var answerBtn3 = document.querySelector("#answer-button-3");
-var answerBtn4 = document.querySelector("#answer-button-4");
-var answerCheck = document.querySelector("#check-answer");
+var checkLine = document.querySelector("#check_line");
+var scoreBoard = document.querySelector("#submit_page");
+var finalScore = document.querySelector("#final_score");
+var userInitial =document.querySelector("#initial");
 
-var completeScreen = document.querySelector("#complete-screen");
-var score = document.querySelector("#final-score");
-var userInitial = document.querySelector("#enter-initials");
-var submitBtn = document.querySelector("#submit-button");
+var submitBtn =document.querySelector("#submit_btn");
+var highScorePage =document.querySelector("#highscore_page");
+var scoreRecord =document.querySelector("#score_record");
+var scoreCheck =document.querySelector("#score_check");
+var finish =document.querySelector("#finish");
 
-var highscorePage = document.querySelector("#highscore-page");
-var scoreRecord = document.querySelector("#score-record");
-var backBtn = document.querySelector("#back-button");
-var clearBtn = document.querySelector("#clear-button");
+var backBtn =document.querySelector("#back_btn");
+var clearBtn=document.querySelector("#clear_btn");
+
 
 var questionSource = [
     {
-        question: "String values must be enclosed within _____ when being assigned to variables.",
+        question: "Questions 1 : String values must be enclosed within _____ when being assigned to variables.",
         choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
         answer: "c"
     },
     {
-        question: "Commonly used data types DO NOT include:",
+        question: "Questions 2 : Commonly used data types DO NOT include:",
         choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
         answer: "c"
     },
     {
-        question: "The first index of an array is ____.",
-        choices: ["a. 1", "b. 8", "c. any", "d. 0"],
-        answer: "d"
+        question: "Questions 5 : To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b"
     },
     {
-        question: "Which event occurs when the user clicks on an HTML element?",
-        choices: ["a. onclick", "b. onchange", "c. onmouseclick", "d. click"],
+        question: "Questions 6 : The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
         answer: "a"
     },
     {
-        question: "What color is #FFFFFF?",
-        choices: ["a. black", "b. white", "c. grey", "d. gold"],
-        answer: "b"
+        question: "Questions 8 : Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a"
     }
-]
+];
 
-var timeLeft = document.getElementById("time");
+
+var timeLeft = document.getElementById("timer");
 
 var secondsLeft = 60;
 var questionNumber = 0;
@@ -61,111 +63,171 @@ var totalScore = 0;
 var questionCount = 1;
 
 function countdown() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeLeft.textContent = "Timer:" + secondsLeft + "s";
+        
+        var timerInterval = setInterval(function () {
 
-        if(secondsLeft <=0){
-            clearInterval(timerInterval);
-            timeLeft.textContent = "Time is up!";
-            finish.textContent = "Time is up!";
-            gameOver();
-        } 
-        else if(questionCount >= questionSource.length +1) {
-            clearInterval(timerInterval);
-            gameOver();
-        }
+          secondsLeft--;
+          timeLeft.textContent = "Time left: " + secondsLeft + " s";
+    
+            if (secondsLeft <= 0){
+                clearInterval(timerInterval);
+                timeLeft.textContent = "Time is up!"; 
+                
+                finish.textContent = "Time is up!";
+                gameOver();
+
+            } else  if(questionCount >= questionSource.length +1) {
+                clearInterval(timerInterval);
+                gameOver();
+                } 
     }, 1000);
-    console.log(countdown)
 }
 
-
-function startQuiz() {
-    welcome.style.display ="none";
-    questionScreen.style.display ="block";
-    questionNumber = 0;
-    countdown();
-    showQuestion(questionNumber);
-    console.log(startQuiz)
+function startQuiz () {
+        introPage.style.display = "none";
+        questionPage.style.display = "block";
+        questionNumber = 0
+        countdown();
+        showQuestion(questionNumber);
+      
 }
 
-function showQuestion(n) {
-    question.textContent = questionSource[n].question;
-    answerBtn1.textContent = questionSource[n].choices[0];
-    answerBtn2.textContent = questionSource[n].choices[1];
-    answerBtn3.textContent = questionSource[n].choices[2];
-    answerBtn4.textContent = questionSource[n].choices[3];
-    questionNumber = n;
-}
+function showQuestion (n) {
+        askQuestion.textContent = questionSource[n].question;
+        answerBtn1.textContent = questionSource[n].choices[0];
+        answerBtn2.textContent = questionSource[n].choices[1];
+        answerBtn3.textContent = questionSource[n].choices[2];
+        answerBtn4.textContent = questionSource[n].choices[3];
+        questionNumber = n;
+    }
 
-function answerCheck (event) {
+function checkAnswer(event) {
     event.preventDefault();
-    answerCheck.style.display = "block";
+    checkLine.style.display = "block";
     setTimeout(function () {
-        answerCheck.style.display = "none";
+        checkLine.style.display = 'none';
     }, 1000);
 
     if (questionSource[questionNumber].answer == event.target.value) {
-        answerCheck.textContent = "Correct";
-        totalScore = totalScore +1;
-    } else {
-        secondsLeft =secondsLeft -10;
-        answerCheck.textContent = "Wrong. The correct answer is" + questionSource[questionNumber].answer + " .";
-    }
+        checkLine.textContent = "Correct!"; 
+        totalScore = totalScore + 1;
 
-    if (questionNumber < questionSource.length -1) {
+    } else {
+        secondsLeft = secondsLeft - 10;
+        checkLine.textContent = "Wrong! The correct answer is " + questionSource[questionNumber].answer + " .";
+    }
+    if (questionNumber < questionSource.length -1 ) {
         showQuestion(questionNumber +1);
     } else {
-        gameOver();
-    }
-   questionCount++; 
+    gameOver();
 }
-
+questionCount++;
+}
 function gameOver() {
-    questionScreen.style.display = "none";
-    highscorePage.style.display = "block";
-    console.log(scoreBoard);
-    score.textContent = "Your Score:" + totalScore;
-    timeLeft.style.display = "none";
-}
+
+        questionPage.style.display = "none";
+        scoreBoard.style.display = "block";
+        console.log(scoreBoard);
+        finalScore.textContent = "Your final score is :" + totalScore ;
+        timeLeft.style.display = "none"; 
+};
 
 function getScore () {
-    var currentList = localStorage.getItem("ScoreList");
-    if (currentList !== null) {
+    var currentList =localStorage.getItem("ScoreList");
+    if (currentList !== null ){
         freshList = JSON.parse(currentList);
-            return freshList;
+        return freshList;
     } else {
         freshList = [];
     }
     return freshList;
-}
+};
+
 
 function renderScore () {
     scoreRecord.innerHTML = "";
-    scoreRecord.style.display = "block";
-    var highScores = sort();
+    scoreRecord.style.display ="block";
+    var highScores = sort();   
+
     var topFive = highScores.slice(0,5);
     for (var i = 0; i < topFive.length; i++) {
         var item = topFive[i];
-    
+
     var li = document.createElement("li");
-    li.textContent = item.userInitial + "-" + item.score;
+    li.textContent = item.user + " - " + item.score;
     li.setAttribute("data-index", i);
-    scoreRecord.appendChild(li);    
+    scoreRecord.appendChild(li);
     }
+};
 
+
+function sort () {
+    var unsortedList = getScore();
+    if (getScore == null ){
+        return;
+    } else{
+    unsortedList.sort(function(a,b){
+        return b.score - a.score;
+    })
+    return unsortedList;
+}};
+
+
+function addItem (n) {
+    var addedList = getScore();
+    addedList.push(n);
+    localStorage.setItem("ScoreList", JSON.stringify(addedList));
+};
+
+function saveScore () {
+    var scoreItem ={
+        user: userInitial.value,
+        score: totalScore
+    }
+    addItem(scoreItem);
+    renderScore();
 }
-
-
-
 
 
 startBtn.addEventListener("click", startQuiz);
 
-reactButton.forEach(function(click) {
-    click.addEventListener("click", answerCheck);
+reactButtons.forEach(function(click){
+
+    click.addEventListener("click", checkAnswer);
+});
+
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    scoreBoard.style.display = "none";
+    introPage.style.display = "none";
+    highScorePage.style.display = "block";
+    questionPage.style.display ="none";
+    saveScore();
+});
+
+scoreCheck.addEventListener("click", function(event) {
+    event.preventDefault();
+    scoreBoard.style.display = "none";
+    introPage.style.display = "none";
+    highScorePage.style.display = "block";
+    questionPage.style.display ="none";
+    renderScore();
 });
 
 
+backBtn.addEventListener("click",function(event){
+        event.preventDefault();
+        scoreBoard.style.display = "none";
+        introPage.style.display = "block";
+        highScorePage.style.display = "none";
+        questionPage.style.display ="none";
+        location.reload();
+});
+
+clearBtn.addEventListener("click",function(event) {
+    event.preventDefault();
+    localStorage.clear();
+    renderScore();
+});
 
 
